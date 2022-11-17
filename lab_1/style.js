38d19e5f3b4db15802ba615d99726950450722c1
azimovin01 <azimovin01@yandex.ru>
2022-11-18 00:31:11 +0300

    js

---
+++ lab_1/style.js
@@ -0,0 +1,14 @@
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
