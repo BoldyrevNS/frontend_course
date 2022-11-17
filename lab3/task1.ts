f3e4ce637bbb956fc4527c41f119529a624abca7
Andrey Zimovin <113522501+ZimovinAA2022pv191@users.noreply.github.com>
2022-11-18 01:14:35 +0300

    Update task1.ts

--- lab3/task1.ts
+++ lab3/task1.ts
@@ -1,1 +1,4 @@
+//Реализовать функцию Array.includes из JavaScript в системе типов
+//TypeScript. Этот тип принимает два тип параметра. Результатом должно
+//быть либо true, либо false.
 type Includes<T extends unknown[], U> = U extends T[number] ? true : false;
