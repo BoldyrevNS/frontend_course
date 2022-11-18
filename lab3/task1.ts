83042e88da2e3af9c73bef24cc497d7455c419cd
artemovaka22pv191 <xenia.artiomowa@yandex.ru>
2022-11-18 14:49:43 +0300

    3Lab

--- lab3/task1.ts
+++ lab3/task1.ts
@@ -3,8 +3,8 @@
 
 type Flatten<T> = T extends []
     ? []
-    : T extends [infer H, ...infer T]
-        ? [...Flatten<H>, ...Flatten<T>]
+    : T extends [infer NewT, ...infer T]
+        ? [...Flatten<NewT>, ...Flatten<T>]
         : [T];
 
-type flatten = Flatten<[[1, 2],[], [3, 4], [[[5]]]]> // [1, 2, 3, 4, 5]
+type flatten = Flatten<[[1, 2], [], [3, 4], [[[5]]]]> // [1, 2, 3, 4, 5]
