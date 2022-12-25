a5254f742ff42d80890fd3880c83026272753e21
Lev <freedomlove6574@gmail.com>
2022-12-25 23:17:24 +0300

    some minor edit

--- lab3/script.ts
+++ lab3/script.ts
@@ -1,15 +1,26 @@
 // npx tsc
 
-type First = Array<any>
+class First{
+    private readonly arr: Array<any>
+
+    constructor(arr: any) {
+        this.arr = arr;
+    }
+    _exec(): any{
+        return typeof this.arr[0]
+    }
+
+}
 
 const stringArray: Array<string> = ['a', 'b', 'c']
 const numberArray: Array<number> = [1, 2, 3]
 const booleanArray: Array<boolean> = [true, false, true]
 
-const str: First = stringArray
-const num: First = numberArray
-const bln: First = booleanArray
+const str = new First(stringArray)._exec()
+const num = new First(numberArray)._exec()
+const boo = new First(booleanArray)._exec()
 
-console.log(typeof str[0])
-console.log(typeof num[0])
-console.log(typeof bln[0])
+console.log(str)
+console.log(num)
+console.log(boo)
+
