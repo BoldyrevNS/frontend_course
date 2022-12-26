39a613153692482174dfb335e91f6f7560a2de1f
Lev <freedomlove6574@gmail.com>
2022-12-26 22:36:27 +0300

    full refactor

--- lab3/script.ts
+++ lab3/script.ts
@@ -1,26 +1,5 @@
-// npx tsc
+type First<T extends Array<any>> = T[0]
 
-class First {
-    private readonly arr: Array<any>
-
-    constructor(arr: any) {
-        this.arr = arr;
-    }
-
-    _exec(): any {
-        return typeof this.arr[0]
-    }
-}
-
-const stringArray: Array<string> = ['a', 'b', 'c']
-const numberArray: Array<number> = [1, 2, 3]
-const booleanArray: Array<boolean> = [true, false, true]
-
-const str = new First(stringArray)._exec()
-const num = new First(numberArray)._exec()
-const boo = new First(booleanArray)._exec()
-
-console.log(str)
-console.log(num)
-console.log(boo)
-
+type aType = First<["1111", "2"]>
+type bType = First<[31, 3]>
+type cType = First<[[1,2], [3,4], [5,6]]>
